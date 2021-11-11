f2909d941dc80e77cab52f110abc54bec8457c4d
DESKTOP-0AE1U7T\andre <anfevo01@gmail.com>
2021-11-10 22:27:06 -0500

    Prueba

---
+++ Grupo10/app/src/main/java/com/example/grupo10/ui/home/HomeFragment.java
@@ -0,0 +1,30 @@
+package com.example.grupo10.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.grupo10.R;
+import com.example.grupo10.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_home,container, false);
+
+        return root;
+    }
+
+
+}
